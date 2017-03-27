2d30fd52b93b9ecdb32e06cc7bdd015de6624d90
victoria.poots <victoriapoots@outlook.com>
2017-03-27 22:06:22 +0100

    Prototype 1
    API calls and map displaying

---
+++ mapApp/qradar_appfw_venv/Scripts/easy_install-2.7-script.py
@@ -0,0 +1,10 @@
+#!C:\mapApp\qradar_appfw_venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==17.0','console_scripts','easy_install-2.7'
+__requires__ = 'setuptools==17.0'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('setuptools==17.0', 'console_scripts', 'easy_install-2.7')()
+    )
